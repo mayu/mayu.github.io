e8420bfb236e7b7c8c3ad1ec9548e172c1efd32d
Sartaj <sartaj@sartaj.me>
2017-04-28 19:46:48 -0500

    add accelerometer capabilities

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2017, Codrops
  * http://www.codrops.com
  */
@@ -138,7 +138,7 @@
 	function removeTilt() {
 		tilt = false;
 	}
-	
+
 	function move(opts) {
 		return new Promise(function(resolve, reject) {
 			if( isMoving && !opts.stopTransition ) {
@@ -161,7 +161,7 @@
 			else {
 				resolve();
 			}
-			
+
 		});
 	}
 
@@ -176,13 +176,13 @@
 						// transform values
 						rotX = tiltRotation.rotateX ? initTransform.rotateX -  (2 * tiltRotation.rotateX / win.height * mousepos.y - tiltRotation.rotateX) : 0,
 						rotY = tiltRotation.rotateY ? initTransform.rotateY -  (2 * tiltRotation.rotateY / win.width * mousepos.x - tiltRotation.rotateY) : 0;
-			
+
 					// apply transform
 					applyRoomTransform({
-						'translateX' : initTransform.translateX, 
-						'translateY' : initTransform.translateY, 
-						'translateZ' : initTransform.translateZ, 
-						'rotateX' : rotX + 'deg', 
+						'translateX' : initTransform.translateX,
+						'translateY' : initTransform.translateY,
+						'translateZ' : initTransform.translateZ,
+						'rotateX' : rotX + 'deg',
 						'rotateY' : rotY + 'deg',
 						'rotateZ' : initTransform.rotateZ
 					});
@@ -192,7 +192,58 @@
 			debounceResizeFn = debounce(function() {
 				win = {width: window.innerWidth, height: window.innerHeight};
 			}, 10);
-		
+
+		// Returns a function, that, when invoked, will only be triggered at most once
+		// during a given window of time. Normally, the throttled function will run
+		// as much as it can, without ever going more than once per `wait` duration;
+		// but if you'd like to disable the execution on the leading edge, pass
+		// `{leading: false}`. To disable execution on the trailing edge, ditto.
+		function throttle(func, wait, options) {
+		  var context, args, result;
+		  var timeout = null;
+		  var previous = 0;
+		  if (!options) options = {};
+		  var later = function() {
+		    previous = options.leading === false ? 0 : Date.now();
+		    timeout = null;
+		    result = func.apply(context, args);
+		    if (!timeout) context = args = null;
+		  };
+		  return function() {
+		    var now = Date.now();
+		    if (!previous && options.leading === false) previous = now;
+		    var remaining = wait - (now - previous);
+		    context = this;
+		    args = arguments;
+		    if (remaining <= 0 || remaining > wait) {
+		      if (timeout) {
+		        clearTimeout(timeout);
+		        timeout = null;
+		      }
+		      previous = now;
+		      result = func.apply(context, args);
+		      if (!timeout) context = args = null;
+		    } else if (!timeout && options.trailing !== false) {
+		      timeout = setTimeout(later, remaining);
+		    }
+		    return result;
+		  };
+		};
+
+		window.ondevicemotion = throttle(function(event) {
+			var accelerationX = event.accelerationIncludingGravity.x.toFixed(1);
+			var accelerationY = event.accelerationIncludingGravity.y.toFixed(1);
+			// apply transform
+			applyRoomTransform({
+				'translateX' : initTransform.translateX*2,
+				'translateY' : initTransform.translateY/2,
+				'translateZ' : initTransform.translateZ,
+				'rotateX' : accelerationY + 'deg',
+				'rotateY' : accelerationX + 'deg',
+				'rotateZ' : initTransform.rotateZ
+			});
+		}, 100);
+
 		document.addEventListener('mousemove', onMouseMoveFn);
 		window.addEventListener('resize', debounceResizeFn);
 
@@ -244,7 +295,7 @@
 			},
 			translateY: function(t, i) {
 				return dir === 'in' ? [150,0] : [0,-150];
-			}	
+			}
 		};
 		if( dir === 'in' ) {
 			animeOpts.begin = function() {
@@ -272,9 +323,9 @@
 			return false;
 		}
 		isNavigating = true;
-		
+
 		var room = DOM.rooms[currentRoom];
-		
+
 		// Remove tilt.
 		removeTilt();
 		// Animate the current slide out - animate the name, title and date elements.
@@ -292,7 +343,7 @@
 		var nextRoom = DOM.rooms[currentRoom];
 		nextRoom.style.transform = 'translate3d(' + (dir === 'next' ? 100 : -100) + '%,0,0) translate3d(' + (dir === 'next' ? 1 : -1) + 'px,0,0)' ;
 		nextRoom.style.opacity = 1;
-		
+
 		// Move back.
 		move({transition: roomTransition, transform: resetTransform})
 		.then(function() {
@@ -317,7 +368,7 @@
 			applyRoomTransition('none');
 			nextRoom.style.transform = 'translate3d(0,0,0)';
 			applyRoomTransform(initTransform);
-			
+
 			setTimeout(function() {
 				initTilt();
 			}, 60);
@@ -392,7 +443,7 @@
 			// Remove adjacent rooms.
 			//removeAdjacentRooms();
 			// Init tilt.
-			initTilt();	
+			initTilt();
 		});
 		anime.remove(DOM.menuItems);
 		anime({
@@ -523,7 +574,7 @@
 		// Show current slide.
 		showSlide(100);
 		// back to room view:
-		move({transform: initTransform, stopTransition: true}).then(function() {	
+		move({transform: initTransform, stopTransition: true}).then(function() {
 			initTilt();
 		});
 
@@ -556,6 +607,9 @@
 		anime(animeInfoOpts);
 	}
 
+	setInterval(function() {
+		document.querySelector('body').style.height = window.innerHeight
+	},100);
 	// Preload all the images.
 	imagesLoaded(DOM.scroller, function() {
 		var extradelay = 1000;
@@ -574,5 +628,4 @@
 			}
 		});
 	});
-
 })(window);
